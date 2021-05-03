6805711159bcfe1db8eb38ab728c8bb86030d18c
yevhensh <yshabala@n-ix.com>
2021-05-03 14:51:57 +0300

    Added sbt project setup

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,10 +1,15 @@
 import sbt._
 
 object Dependencies {
+  object PureConfig {
+    private val pureConfigVersion = "0.15.1"
+    val pureconfig                = "com.github.pureconfig" %% "pureconfig" % pureConfigVersion
+  }
+
   object Http4s {
     private val http4sVersion = "0.21.22"
-    val dsl         = "org.http4s" %% "http4s-dsl"          % http4sVersion
-    val blazeServer = "org.http4s" %% "http4s-blaze-server" % http4sVersion
-    val circe       = "org.http4s" %% "http4s-circe"        % http4sVersion
+    val dsl                   = "org.http4s" %% "http4s-dsl" % http4sVersion
+    val blazeServer           = "org.http4s" %% "http4s-blaze-server" % http4sVersion
+    val circe                 = "org.http4s" %% "http4s-circe" % http4sVersion
   }
 }
